2be9acb16e562c313a79d46714181421e3fa248d
Callum Wilson <callum.w@outlook.com>
2017-12-12 21:50:10 +0000

    Modified gitignore to ignore build directory. Implemented using params and tightened up code

--- src/main.c
+++ src/main.c
@@ -2,37 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define DEFAULT_PORT 5000
 
-int stop;
+/** GLOBALS **/
+char g_ip_addr[16]; // xxx.xxx.xxx.xxx\0 <- 16 chars
+uint16_t g_port = 0;
+struct ip_mreq g_mreq;
+struct sockaddr_in g_name;
+bool g_stop = 0;
+int g_socket = 0;
+
 void signal_handler(int signum)
 {
-    stop = 1;
+    g_stop = 1;
     printf(" Received stop signal\nStopping....\n");
 }
 
-int create_socket(uint16_t port)
+int create_socket()
 {
-    int sock;
-    struct sockaddr_in name;
-
-    sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
+    g_socket = socket(PF_INET, SOCK_DGRAM, 0);
+    if (g_socket < 0) {
         perror("socket");
-        exit(EXIT_FAILURE);
+        return -1;
     }
-    name.sin_family = AF_INET;
-    name.sin_port = htons(port);
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(sock, (struct sockaddr*) &name, sizeof(name)) < 0) {
+    if (bind(g_socket, (struct sockaddr*) &g_name, sizeof(g_name)) < 0) {
         perror("bind");
-        exit(EXIT_FAILURE);
+        return -1;
     }
-    return sock;
+    return g_socket;
 }
 
 int close_socket(int sock)
@@ -42,10 +46,7 @@ int close_socket(int sock)
 
 int join_multicast(char** ip, int socket)
 {
-    struct ip_mreq mreq;
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-    inet_aton(ip, &mreq.imr_multiaddr);
-    if (setsockopt(socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+    if (setsockopt(socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &g_mreq, sizeof(g_mreq)) < 0) {
         perror("join multicast");
         return -1;
     }
@@ -57,44 +58,92 @@ int leave_multicast()
     return -1;
 }
 
-int main(int argc, char** argv)
+bool set_ip(char* ip)
 {
-    char* addr = NULL;
-    char* port = NULL;
+    if ( ip != NULL) {
+        if ( inet_aton(ip, &g_mreq.imr_multiaddr) != 0 ){
+            strncpy(g_ip_addr, ip, 15); // copy address, leave last element as null terminator
+            return true;
+        }
+    }
+    else {
+        fprintf(stderr, "%s was passed a null pointer\n", __func__);
+        return false;
+    }
+}
+
+bool parse_options(int argc, char** argv)
+{
+    if ( argv == NULL ) return -1;
+    bool got_ip = false;
+    bool got_port = false;
+    int tmp;
     int opt;
     while ((opt = getopt(argc, argv, "a:p:")) != -1) {
         switch (opt) {
-            case 'a':
-                addr = optarg;
+            case 'a':   // ip address
+                if ( !set_ip(optarg) ) {
+                    fprintf(stderr, "%s is not a valid ip address\n", optarg);
+                    return false;
+                }
+                got_ip = true;
                 break;
-            case 'p':
-                port = optarg;
+            case 'p':   // port number
+                tmp = atoi(optarg);
+                if ( tmp >= 0 && tmp <= 65535 )
+                    g_port = (uint16_t) tmp;
+                else {
+                    fprintf(stderr, "%s is not a valid port, using %d instead\n", optarg, DEFAULT_PORT);
+                    g_port = DEFAULT_PORT;
+                }
+                g_name.sin_port = htons(g_port);
+                got_port = true;
                 break;
-            case '?':
+            case '?':   // unknown
                 switch (optopt) {
                     case 'a':
                         break;
                     case 'p':
                         break;
                     default:
-                        printf("Unknown option -%c\n", optopt);
+                        fprintf(stderr, "Unknown option -%c\n", optopt);
                 }
-                return EXIT_FAILURE;
+                return false;
         }
     }
-
-    if (addr == NULL) {
-
+    if ( !got_ip ) {
+        fprintf(stderr, "No ip address specified, exiting.\n");
+        return false;
     }
-    stop = 0;
+    if ( !got_port ) {
+        fprintf(stderr, "No port specified, using port %d\n", DEFAULT_PORT);
+        g_port = DEFAULT_PORT;
+    }
+    return true;
+}
+
+bool init()  // Must be called after globals are set up
+{
+    g_mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    g_name.sin_addr.s_addr = htonl(INADDR_ANY);
+    g_name.sin_family = AF_INET;
+    if ( create_socket() < 0) {
+        fprintf(stderr, "Failed to create socket\n");
+        return false;
+    }
     signal(SIGINT, signal_handler);
-    printf("Transport packets are %d bytes long!\n", TRANSPORT_PACKET_LENGTH);
-    int socket = create_socket(5000);
-    printf("Created socket!\n");
-    while (!stop) {
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if ( !parse_options(argc, argv) ) return EXIT_FAILURE;
+    if ( !init() ) return EXIT_FAILURE;
+
+    while (!g_stop) {
         sleep(1);
     }
-    close_socket(socket);
+    close_socket(g_socket);
     printf("Socket closed\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
